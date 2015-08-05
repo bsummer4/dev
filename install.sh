0b25e5d26d3f629d584f3b6633fa94187564b7e6
Benjamin Summers <ben@benjaminsummers.net>
2015-08-05 14:40:47 +0000

    Updated for use outside of nixos.

--- install.sh
+++ install.sh
@@ -3,5 +3,10 @@
 cd dotfiles
 
 for x in *
-do (cd ~; ln -s dev/dotfiles/$x .$x)
+do (cd ~; ln -f -s dev/dotfiles/$x .$x)
 done
+
+basics="curl gitAndTools.gitFull gnugrep gzip less mosh parted psmisc tmux vim wget"
+general="discount gnumake gnupg gnused gnutar graphviz jq mercurial screen silver-searcher w3m rlwrap openvpn"
+
+nix-env -iA $(for pkg in $basics $general heroku; do echo nixpkgs.$pkg; done)
--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-let haskellPackages = pkgs.haskellPackages.override {
-      extension = self: super: {
-        Foo = self.callPackage ./. {};
-      };
-    };
- in lib.overrideDerivation haskellPackages.Foo (attrs: {
-      buildInputs = [ haskellPackages.cabalInstall ] ++ attrs.buildInputs;
-    })
